9c39cb8cf0b9563591d0713883394fff5374591c
Kauetxp <123607569+Kauetxp@users.noreply.github.com>
2023-03-22 22:09:26 +0000

    AAAAAA

--- ex1.py
+++ ex1.py
@@ -1,1 +1,10 @@
-# oio
+#Escreva um programa que peça ao usuário para digitar uma lista de números e, em seguida, imprima apenas os números pares da lista.
+
+lista = []
+
+i = 0
+while i < 10:
+    n = input("Digite um número: ")
+    lista.append(n)
+    i+=1
+print(lista)
